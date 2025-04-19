08fedf092b9dfbbcb3acf48e641bcff5ec2fa882
FrankFeitosa <feitosfrank22@gmail.com>
2025-04-19 15:32:23 -0300

    Finalizado o projeto de gerenciamento de tarefas

--- funcoes.js
+++ funcoes.js
@@ -3,20 +3,22 @@ import PromptSync from 'prompt-sync';
 const tarefas = '../task-management-system/tarefas.json'
 const prompt = PromptSync()
 
-export function menu () {
+export function menu() {
     return `
-        Menu  de operações de tarefas:
-        1 - Visualise uma tarefa.
-        2 - Crie uma tarefa.
-        3 - Atualize uma tarefa.
-        4 - Concluir tarefa.
+        Menu de tarefas:
+        1 - Crie uma tarefa.
+        2 - Visuaçise todas as tarefas.
+        3 - Visualise as tarefas concluídas.
+        4 - Visualise as tarefas não concluídas.
+        5 - Concluir tarefa.
+        6 - Sair.
         `
 }
 
 export async function lerTarefas() {
     try {
         const dados = await fs.readFile(tarefas, 'utf-8');
-        return JSON.parse(dados) 
+        return JSON.parse(dados)
     } catch (error) {
         console.error(`Erro ao ler o arquivo ${error.message}`)
         return undefined
@@ -24,8 +26,8 @@ export async function lerTarefas() {
 }
 
 export async function escreverNoArquivo(listaTarefas) {
-    const listaTarefasString = JSON.stringify(listaTarefas, null, 2)
     try {
+        const listaTarefasString = JSON.stringify(listaTarefas, null, 2)
         await fs.writeFile(tarefas, listaTarefasString, 'utf-8')
         return "Arquivo Atualizado"
     } catch (error) {
@@ -34,39 +36,91 @@ export async function escreverNoArquivo(listaTarefas) {
     }
 }
 
+export async function criarTarefas() {
+    try {
+        const tarefa = await lerTarefas()
+        const titulo = prompt("Digite o título da tarefa: ")
+        const descricao = prompt("Descreva sua tarefa: ")
+        
+        const idNovo = tarefa[tarefa.length-1].id + 1;
+
+        const novaTarefa = {
+            id: idNovo,
+            titulo: titulo,
+            descricao: descricao,
+            concluida: false
+        }
+
+        tarefa.push(novaTarefa)
+        await escreverNoArquivo(tarefa)
+    } catch (error) {
+        console.error(`
+        Erro: ${error}
+        `)
+    }
+}
+
 export async function mostrarTarefas() {
     try {
         const todasAsTarefas = await lerTarefas();
-        console.log( await todasAsTarefas )
+        console.log(await todasAsTarefas)
     } catch (error) {
         console.error(`Erro ao mostrar o arquivo ${error.message}`)
         return undefined
     }
 }
 
-export async function tarefasConcluidas(){
-    const tarefa = await lerTarefas()
-    const realizadas = tarefa.filter(r => r.concluida == true)
-    if(!realizadas){
-        console.log("Não Encontradas!")
+export async function tarefasConcluidas() {
+    try {
+        const tarefa = await lerTarefas()
+        const realizadas = tarefa.filter(r => r.concluida == true)
+        if (!realizadas) {
+            console.log("Não Encontradas!")
+        }
+        console.log(`Tarefas concluídas com sucesso: ${realizadas.forEach((item, index) => {
+            console.log(` ${index + 1} => Titulo - ${item.titulo} | Descrição - ${item.descricao} | Concluídas - ${item.concluida}`)
+        })}`)
+    } catch (error) {
+        console.error(`
+            Erro: ${error}
+            `)
     }
-    console.log(`Tarefas concluídas com sucesso: ${realizadas.forEach( (item, index) => {
-    console.log(` ${index + 1} => Titulo - ${item.titulo} | Descrição - ${item.descricao} | Concluídas ${item.concluida}`)        
-    })}`)
 }
-export async function criarTarefas() {
-    const tarefa = await lerTarefas()
-    const titulo = prompt("Digite o título da tarefa: ")
-    const descricao = prompt("Descreva sua tarefa: ")
-    const idNovo = tarefa[tarefa.length-1].id + 1;
 
-    const novaTarefa = {
-        id: idNovo,
-        titulo,
-        descricao,
-        concluida: false
+export async function tarefasNaoConcluidas() {
+    try {
+        const tarefa = await lerTarefas()
+        const naoConcluidas = tarefa.filter((nr) => nr.concluida == false)
+        if (!naoConcluidas) {
+            console.log(`Tarefas concluidas ${await tarefasConcluidas()}`)
+        }
+        console.log(` Tarefas não concluídas: ${naoConcluidas.forEach((item, index) => {
+            console.log(`
+            ${index + 1} => Titulo - ${item.titulo} | Descrição - ${item.descricao} | Concluídas - ${item.concluida}  
+            `)
+        })}`)
+    } catch (error) {
+        console.error(`
+        Erro: ${error}
+        `)
+    }
+}
+
+export async function tarefaAConcluir() {
+    try {
+        const concluir = +prompt("Qual tarefa você quer atualizar como concluída. Escreva o id: ")
+        const tarefa = await lerTarefas()
+
+        const tarefaAConcluir = tarefa.find(conclui => conclui.id === concluir)
+        if (tarefaAConcluir) {
+            tarefaAConcluir.concluida = true
+            await escreverNoArquivo(tarefa)
+            console.log(" Tarefa concluída com sucesso!")
+        }
+    } catch (error) {
+        console.error(`
+            Erro: ${error} 
+            `)
     }
 
-    tarefa.push(novaTarefa)
-    await escreverNoArquivo(tarefa)
 }
--- main.js
+++ main.js
@@ -1,13 +1,33 @@
-import {menu, lerTarefas, escreverNoArquivo, mostrarTarefas, criarTarefas, tarefasConcluidas} from './funcoes.js'
+import { menu, mostrarTarefas, criarTarefas, tarefasConcluidas, tarefasNaoConcluidas, tarefaAConcluir } from './funcoes.js'
+import PromptSync from 'prompt-sync';
+const prompt = PromptSync()
 
-// console.log(menu());
+let item;
+console.log(menu())
+do {
+    item = +prompt("Escolha um dos itens do Gerenciador de Tarefa: ")
+    switch (item) {
+        case 1:
+            await criarTarefas()
+            break;
+        case 2:
+            await mostrarTarefas()
+            break;
+        case 3:
+            await tarefasConcluidas()
+            break;
+        case 4:
+            await tarefasNaoConcluidas()
+            break;
+        case 5:
+            await tarefaAConcluir()
+            break;
+        case 6:
+            console.log("Sair do Gerenciador")
+            break;
+        default:
+            console.log("Opção inválida. Tente outra vez.")
+            break;
+    }
+} while (item !== 6);
 
-// console.log(await lerTarefas())
-
-// await escreverNoArquivo();
-
-// await mostrarTarefas()
-
-// await criarTarefas()
-
-await tarefasConcluidas()
